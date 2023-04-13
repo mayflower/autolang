3c03cf560d9a1a5e15663ef5114f0a1e784671bd
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2023-04-13 14:11:09 +0200

    llamacpp instead of openai

--- autolang/__main__.py
+++ autolang/__main__.py
@@ -6,6 +6,8 @@ from langchain import FAISS, InMemoryDocstore
 from langchain.agents import Tool, load_tools
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import LlamaCpp
+from langchain.embeddings import LlamaCppEmbeddings
 from langchain.llms.base import BaseLLM 
 
 from .auto import AutoAgent
@@ -15,17 +17,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API Keys
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", default="")
-assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+LLAMA_MODEL = os.getenv("LLAMA_MODEL", default="alpaca-13b-ggml-q4_0-lora-merged/ggml-model-q4_0.bin")
+assert LLAMA_MODEL, "LLAMA_MODEL environment variable is missing from .env"
 
-OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", default="gpt-3.5-turbo")
-assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
+
+llm: BaseLLM = LlamaCpp(model_path=LLAMA_MODEL, temperature=0, echo=True, n_ctx=2048)
+embeddings = LlamaCppEmbeddings(model_path=LLAMA_MODEL) # type: ignore
+print(embeddings)
 
 objective = input('What is my purpose? ')
-
-
-llm: BaseLLM = ChatOpenAI(model_name=OPENAI_API_MODEL, temperature=0, request_timeout=120) # type: ignore 
-embeddings = OpenAIEmbeddings() # type: ignore
 
 """
 Customize the tools the agent uses here. Here are some others you can add:
@@ -36,7 +36,7 @@ os.environ["SERPER_API_KEY"] = "<KEY>"
 tool_names = ["terminal", "requests", "python_repl", "human", "google-serper", "wolfram-alpha"]
 """
 
-tool_names = ["python_repl", "human"]
+tool_names = ["python_repl", "human", "google-serper", "requests"]
 
 tools: List[Tool] = load_tools(tool_names, llm=llm)  # type: ignore
 
